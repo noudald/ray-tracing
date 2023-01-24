6be3c137a908e20be54bf37e6a90a1bedfe245aa
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-01-24 06:56:11 +0100

    Reorganizing code and using cmakelists

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <vec3.h>
+
 int main() {
     // Image settings.
     const int image_width = 256;
@@ -9,6 +11,7 @@ int main() {
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; j--) {
+        std::cerr << ".";
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -21,4 +24,8 @@ int main() {
             std::cout << ir << " " << ig << " " << ib << std::endl;
         }
     }
+    std::cerr << std::endl;
+
+    Vec3 vec(1.0, 2.0, 3.0);
+    std::cout << vec << " " << dot(vec, vec) << std::endl;
 }
---
+++ c++/src/vec3.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <cmath>
+
+using std::sqrt;
+
+class Vec3 {
+    public:
+        Vec3(): vec{0, 0, 0} {}
+
+        Vec3(float e0, float e1, float e2): vec{e0, e1, e2} {}
+
+        float x() const {
+            return vec[0];
+        }
+
+        float y() const {
+            return vec[1];
+        }
+
+        float z() const {
+            return vec[2];
+        }
+
+        Vec3 operator-() const {
+            return Vec3(-vec[0], -vec[1], -vec[2]);
+        }
+
+        float operator[](int i) const {
+            return vec[i];
+        }
+
+        float& operator[](int i) {
+            return vec[i];
+        }
+
+        Vec3& operator+=(const Vec3 &other) {
+            vec[0] += other[0];
+            vec[1] += other[1];
+            vec[2] += other[2];
+        }
+
+        Vec3& operator*=(const float t) {
+            vec[0] *= t;
+            vec[1] *= t;
+            vec[2] *= t;
+        }
+
+        Vec3& operator/=(const float t) {
+            return *this *= 1/t;
+        }
+
+        float dot(const Vec3 &other) {
+            return vec[0]*other[0] + vec[1]*other[1] + vec[2]*other[2];
+        }
+
+        float length_squared() const {
+            return vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2];
+        }
+
+        float length() const {
+            return sqrt(length_squared());
+        }
+
+    private:
+        float vec[3];
+};
+
+using Point3 = Vec3;
+using Color = Vec3;
+
+inline std::ostream& operator<<(std::ostream &out, const Vec3 &vec) {
+    return out << "(" << vec.x() << ", " << vec.y() << ", " << vec.z() << ")";
+}
+
+float dot(const Vec3 &u, const Vec3 &v);
