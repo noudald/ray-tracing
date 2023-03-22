df7674d495d4b4b223db8c0aa6a04a1849841fc8
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-03-22 14:21:49 +0100

    Remove rust code and start over

--- rust/src/color.rs
+++
@@ -1,9 +0,0 @@
-use crate::vec3::Color;
-
-pub fn write_color(pixel_color: &Color) {
-    let ir = (255.999 * pixel_color.x) as u32;
-    let ig = (255.999 * pixel_color.y) as u32;
-    let ib = (255.999 * pixel_color.z) as u32;
-
-    println!("{} {} {}", ir, ig, ib);
-}
